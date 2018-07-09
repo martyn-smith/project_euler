ab53b947d6eb98834b14dc6253e55334990409e1
martyn-smith <martyn.smith560@gmail.com>
2018-07-09 17:30:35 +0100

    renaming files for quicker/easier typing.

--- python/euler15.py
+++
@@ -1,9 +0,0 @@
-"""
-This doesn't really seem like a problem for a computer...
-"""
-
-def Euler_15():
-    return "go work it out yourself, dum-dum"
-
-if __name__ == "__main__":
-    print(Euler_15())
