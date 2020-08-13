1ee665db145e4002aafb14c7292c3bcb35a82cca
Martyn <martyn.smith560@outlook.com>
2020-08-13 15:59:16 +0100

    refactored e9 to return Option()

--- rust/e9.rs
+++ rust/e9.rs
@@ -3,21 +3,18 @@ There is one pythagorean triple whose sum is 1000 (i.e. a + b + c = 1000, and a^
 Returns the product of that triple.
 */
 
-fn euler_9() -> usize {
-    //TODO: return this as a Result<usize, TripleNotFound>
-    let mut product : usize = 0;
-
+fn euler_9() -> Option<usize> {
     for a in 1..1000 {
         for b in 1..(1000-a) {
             let c : usize = 1000 - (a + b);
             if a.pow(2) == b.pow(2) + c.pow(2) {
-                product = a * b * c;
+                return Some(a * b * c);
             }
         }
     }
-    product
+    None
 }
 
 fn main() {
-    println!("{}", euler_9());
+    println!("{}", euler_9().expect("no solution found"));
 }
