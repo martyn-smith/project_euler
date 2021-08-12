a695da4d7ecc65520210f2ca9a3b6f2e019110fb
Martyn <martyn.smith560@outlook.com>
2021-08-12 14:27:15 +0100

    Further e1 closure refinement Removed bad Haskell

--- rust/e1.rs
+++ rust/e1.rs
@@ -4,9 +4,10 @@ Find the sum of all the multiples of 3 or 5 below 1000. (fizzbuzz++)
 
 fn euler_1(max_num : usize) -> usize {
     const FIZZERS: [usize; 2] = [3, 5];
+    let fizzable = |x| FIZZERS.iter()
+                              .any(|n| x % n == 0);
     (0..max_num)
-        .filter(|i| FIZZERS.iter()
-                           .any(|f| i % f == 0))
+        .filter(|&i| fizzable(i))
         .sum()
 }
 
